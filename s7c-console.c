c589e250bc694993a060c1967a525f35490cece1
Samega 7Cattac <7Cattac@gmail.com>
2020-12-12 23:16:02 +0000

    c updates
    Signed-off-by: Samega 7Cattac <7Cattac@gmail.com>

--- s7c-console.c
+++ s7c-console.c
@@ -4,20 +4,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-int fast_argcmp(char * a, char * b, int b_len)
+int fast_argcmp(char * a, char * b, unsigned int b_len)
 {
     return *(a + b_len) == '\0' && !memcmp(a, b, b_len);
 }
 
-char ** line_extract(char * line, int * n_words, char ** old_args)
+int sanatized_argcmp(char * a, char * b, unsigned int a_len, unsigned int b_len)
+{
+    return a_len == b_len && !memcmp(a, b, b_len);
+}
+
+char ** line_extract(char * line, unsigned int * n_words, char ** old_args)
 {
     char ** argv = NULL;
     if (old_args) argv = old_args;
     char * word = strtok(line, " \n\r");
+    int word_len = 0;
     while(word)
     {
         argv = realloc(argv, sizeof(char *) * (*n_words + 1));
-        int word_len = strlen(word) + 1;
+        word_len = strlen(word) + 1;
         argv[*n_words] = malloc(sizeof(char) * word_len);
         memcpy(argv[(*n_words)++], word, word_len);
         word = strtok(0, " \n\r");
@@ -25,12 +31,13 @@ char ** line_extract(char * line, int * n_words, char ** old_args)
     return argv;
 }
 
-char ** get_args(char * msg, int * argc, char ** old_args)
+char ** get_args(char * msg, unsigned int * argc, char ** old_args)
 {
     char line[MAX_ARGS] = "\0";
     do
     {
-        if (msg) printf("%s (\"cancel\" to exit):\n", msg);
+        if (msg)
+            printf("%s (\"cancel\" to exit):\n", msg);
         printf(">> ");
         fflush(stdout);
         fgets(line, MAX_ARGS, stdin);
@@ -47,19 +54,36 @@ void free_args(char ** args, int * n_args)
 
 void draw_msg(char ** args, unsigned int offset_draw, unsigned int offset_pos, unsigned int start, unsigned int length, char * msg)
 {
-    for (unsigned int i = 0; i < offset_draw; ++i) printf(" ");
+    for (unsigned int i = 0; i < offset_draw; ++i)
+        printf(" ");
     for (unsigned int it = offset_pos; it < start; ++it)
     {
-        for (unsigned int i = 0; i < strlen(args[it]); ++i) printf(" ");
+        for (unsigned int i = 0; i < strlen(args[it]); ++i)
+            printf(" ");
         printf(" ");
     }
     printf("^");
     for (unsigned int it = start; it < start + length; ++it)
     {
-        for (unsigned int i = 0; i < strlen(args[it]); ++i) printf("~");
+        for (unsigned int i = 0; i < strlen(args[it]); ++i)
+            printf("~");
         printf("~");
     }
-    if (length) printf("\b\b  ");
+    if (length)
+        printf("\b\b  ");
     printf("\n");
     fprintf(stderr,"%s\n", msg);
+}
+
+char ** vectorize_input(char * prefix, unsigned int * argc, char ** old_args)
+{
+    if (!old_args) *argc = 0;
+    char line[MAX_ARGS] = "\0";
+    if (prefix)
+    {
+        printf("%s", prefix);
+        fflush(stdout);
+    }
+    fgets(line, MAX_ARGS, stdin);
+    return line_extract(line, argc, old_args);
 }
--- s7c-console.h
+++ s7c-console.h
@@ -3,14 +3,18 @@
 
 #define MAX_ARGS 255
 
-int fast_argcmp(char * a, char * b, int b_len);
+int fast_argcmp(char * a, char * b, unsigned int b_len);
 
-char ** line_extract(char * line, int * n_words, char ** old_args);
+int sanatized_argcmp(char * a, char * b, unsigned int a_len, unsigned int b_len);
 
-char ** get_args(char * msg, int * argc, char ** old_args);
+char ** line_extract(char* line, unsigned int* n_words, char** old_args);
+
+char ** get_args(char * msg, unsigned int * argc, char ** old_args);
 
 void free_args(char ** args, int * n_args);
 
 void draw_msg(char ** args, unsigned int offset_draw, unsigned int offset_pos, unsigned int start, unsigned int length, char * msg);
+
+char ** vectorize_input(char * prefix, unsigned int * argc, char ** old_args);
 
 #endif
